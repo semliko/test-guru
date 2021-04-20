8e235c30a9bb1df7599a697542bf293503144d32
semliko <megabytus@gmail.com>
2021-04-20 21:02:07 +0100

    set english lang as default

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module TestGuru
     # in config/environments, which are processed later.
     #
     config.time_zone = 'London'
+    config.i18n.default_locale = :en
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
