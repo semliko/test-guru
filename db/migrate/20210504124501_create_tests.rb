6c37911ba4c4b9b59a1a1ba299ea3ac87be8bb83
semliko <megabytus@gmail.com>
2021-05-04 14:00:21 +0100

    default values and not null values added

--- db/migrate/20210504124501_create_tests.rb
+++ db/migrate/20210504124501_create_tests.rb
@@ -1,8 +1,8 @@
 class CreateTests < ActiveRecord::Migration[6.1]
   def change
     create_table :tests do |t|
-      t.string :title
-      t.integer :level
+      t.string :title, null: false
+      t.integer :level, default: 1
       t.references :category, null: false, foreign_key: true
 
       t.timestamps
