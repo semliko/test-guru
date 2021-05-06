709d20d08409478587f2d82fbdfdf0235fc8f823
semliko <megabytus@gmail.com>
2021-05-06 14:39:15 +0100

    order tests by category name

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,4 +2,12 @@ class Test < ApplicationRecord
   belongs_to :category
   has_many :questins
   has_and_belongs_to_many :users
+
+  class << self
+    def sort_by_category(category_title)
+      joins('JOIN categories ON tests.category_id = categories.id')
+        .where('categories.title' => category_title)
+        .order('tests.id DESC')
+    end
+  end
 end
