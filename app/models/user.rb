30cd199d535e2cb0a63303d668b9c3acdad11bc5
semliko <megabytus@gmail.com>
2021-05-10 12:23:40 +0100

    deleted migration AddUserToAnswers and user to answer association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   has_and_belongs_to_many :tests
   has_many :created_tests, class_name: 'Test', foreign_key: 'author_id'
-  has_many :answers
 
   def tests_by_level(level)
     tests.where(level: level)
