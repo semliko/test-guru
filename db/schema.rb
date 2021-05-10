98b929c6a51fa9cb118d53e39677233d9c791816
semliko <megabytus@gmail.com>
2021-05-10 10:55:51 +0100

    added users to answers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_07_195419) do
+ActiveRecord::Schema.define(version: 2021_05_10_094938) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -18,7 +18,9 @@ ActiveRecord::Schema.define(version: 2021_05_07_195419) do
     t.integer "question_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
+    t.index ["user_id"], name: "index_answers_on_user_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -60,6 +62,7 @@ ActiveRecord::Schema.define(version: 2021_05_07_195419) do
   end
 
   add_foreign_key "answers", "questions"
+  add_foreign_key "answers", "users"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
   add_foreign_key "tests", "users", column: "author_id"
