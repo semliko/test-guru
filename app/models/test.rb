c241249b27c57be14e65c0a6782465aa2d8e0857
semliko <megabytus@gmail.com>
2021-05-11 21:17:58 +0100

    replace class methods with scopes

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,15 +3,13 @@ class Test < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :questions
   has_and_belongs_to_many :users
+  scope :by_level, ->(level) { where(level: level) }
   scope :simple_level, -> { where(level: 0..1) }
   scope :middle_level, -> { where(level: 2..4) }
   scope :difficult_level, -> { where(level: 5..Float::INFINITY) }
-
-  class << self
-    def sort_by_category(category_title)
-      joins('JOIN categories ON tests.category_id = categories.id')
-        .where('categories.title' => category_title)
-        .order('tests.id DESC')
-    end
-  end
+  scope :sort_by_category, lambda { |category_title|
+                             joins('JOIN categories ON tests.category_id = categories.id')
+                               .where('categories.title' => category_title)
+                               .order('tests.id DESC')
+                           }
 end
