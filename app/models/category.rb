67e2df6b6ca2009b314fccc702f2d31dea202519
semliko <megabytus@gmail.com>
2021-05-11 20:24:32 +0100

    scopes added

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
   has_many :tests
+  scope :ordered_by_name, -> { order('title ASC') }
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -3,6 +3,9 @@ class Test < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :questions
   has_and_belongs_to_many :users
+  scope :simple_level, -> { where(level: 0..1) }
+  scope :middle_level, -> { where(level: 2..4) }
+  scope :difficult_level, -> { where(level: 5..Float::INFINITY) }
 
   class << self
     def sort_by_category(category_title)
