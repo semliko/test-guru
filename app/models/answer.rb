ef4a2442de5e1dbcbe53f54bd22eddb78ec35032
semliko <megabytus@gmail.com>
2021-05-12 20:57:02 +0100

    fix validation for number of question answers

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,10 @@
 class Answer < ApplicationRecord
   belongs_to :question
   validates :body, presence: true
+  validates :number_of_question_answers
+
+  def number_of_question_answers
+    existing_answers = question.answers.count
+    errors.add(:question, 'Cannot have more then 4 answers') if existing_answers >= 4
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -3,5 +3,4 @@ class Question < ApplicationRecord
   has_many :answers
   scope :correct_answers, -> { where(correct: true) }
   validates :body, presence: true
-  validates :answers, length: { minimum: 1, maximum: 4 }
 end
