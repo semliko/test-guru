39c0c310203850292edcc4ce5d88e6f0620e63b8
semliko <megabytus@gmail.com>
2021-05-12 21:58:12 +0100

    added back test_by_level method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_and_belongs_to_many :tests
   has_many :created_tests, class_name: 'Test', foreign_key: 'author_id'
   validates :email, :name, presence: true
+
+  def tests_by_level(level)
+    tests.where(level: level)
+  end
 end
