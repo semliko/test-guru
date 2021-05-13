2dcfd23fd891faface9b6501d194faa4955e9e03
semliko <megabytus@gmail.com>
2021-05-13 13:42:22 +0100

    callback added to exec answer validation before create

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,7 @@
 class Answer < ApplicationRecord
   belongs_to :question
-  validates :body, presence: true
-  validates :number_of_question_answers
+  validate :body, presence: true, before: :create
+  validate :number_of_question_answers, before: :create
   scope :correct_answers, -> { where(correct: true) }
 
   def number_of_question_answers
