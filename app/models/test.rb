e7677932e6e3006fb6dd00c1bdca3cc27d30fa92
semliko <megabytus@gmail.com>
2021-05-13 13:57:29 +0100

    scope to join with categories added to tests

--- app/models/test.rb
+++ app/models/test.rb
@@ -7,13 +7,13 @@ class Test < ApplicationRecord
   scope :simple_level, -> { where(level: 0..1) }
   scope :middle_level, -> { where(level: 2..4) }
   scope :difficult_level, -> { where(level: 5..Float::INFINITY) }
-  scope :names, -> { pluc :title }
+  scope :join_with_categories, -> { joins('JOIN categories ON tests.category_id = categories.id') }
   validates :title, presence: true
   validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   class << self
     def sort_by_category(category_title)
-      joins('JOIN categories ON tests.category_id = categories.id')
+      join_with_categories
         .where('categories.title' => category_title)
         .order('tests.id DESC')
     end
