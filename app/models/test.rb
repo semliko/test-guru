323c7575425f15728ed7b71dbe3f0b4e590692c3
semliko <megabytus@gmail.com>
2021-05-13 19:41:59 +0100

    fix scope in tests

--- app/models/test.rb
+++ app/models/test.rb
@@ -7,16 +7,17 @@ class Test < ApplicationRecord
   scope :simple_level, -> { where(level: 0..1) }
   scope :middle_level, -> { where(level: 2..4) }
   scope :difficult_level, -> { where(level: 5..Float::INFINITY) }
-  scope :join_with_categories, -> { joins('JOIN categories ON tests.category_id = categories.id') }
+  scope :join_with_categories, lambda { |category_title|
+                                 joins('JOIN categories ON tests.category_id = categories.id').where('categories.title' => category_title)
+                               }
   validates :title, presence: true
   validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates_uniqueness_of :title, scope: :level
 
   class << self
-    def sort_by_category(category_title)
-      join_with_categories
-        .where('categories.title' => category_title)
-        .order('categories.title ASC')
+    def titles_by_category(category_title)
+      join_with_categories(category_title)
+        .order('categories.title ASC').pluck(:title)
     end
   end
 end
