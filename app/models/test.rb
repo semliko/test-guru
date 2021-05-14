b03cbfe2c8e09ba083ba139c8206b170278233e0
semliko <megabytus@gmail.com>
2021-05-14 08:12:24 +0100

    remove old validation method

--- app/models/test.rb
+++ app/models/test.rb
@@ -12,7 +12,6 @@ class Test < ApplicationRecord
                                }
   validates :title, presence: true, uniqueness: { scope: :level }
   validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates_uniqueness_of :title, scope: :level
 
   class << self
     def titles_by_category(category_title)
