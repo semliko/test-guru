d89c417b6f1252c679e7547150b5de3e63e764d1
semliko <megabytus@gmail.com>
2021-05-17 14:33:17 +0100

    method to destroy question added

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -51,7 +51,7 @@ class QuestionsController < ApplicationController
   def destroy
     @question.destroy
     respond_to do |format|
-      format.html { redirect_to questions_url, notice: 'Question was successfully destroyed.' }
+      format.html { redirect_to test_questions_url(@test), notice: 'Question was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
