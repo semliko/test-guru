1780d4a27c225baaa2fb915faf4b43be6809d440
semliko <megabytus@gmail.com>
2021-05-20 14:16:01 +0100

    question helper for edit method

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,5 +1,6 @@
 module QuestionsHelper
-  def question_header(test_title)
-    "Create New #{test_title} Question"
+  def question_header(test_title, question)
+    header_start = question.persisted? ? 'Edit' : 'Create New'
+    "#{header_start} #{test_title} Question"
   end
 end
