572bbce13f2947174ead5ab052e91fd0e6a5553a
semliko <megabytus@gmail.com>
2021-07-16 16:15:34 +0100

    logout and flash helpers added

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/new'
   root 'tests#index'
   get :signup, to: 'users#new'
+  get :login, to: 'sessions#new'
+  get :logout, to: 'sessions#destroy'
   resources :users, only: :create
+  resources :sessions, only: :create
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :tests do
     resources :questions, shallow: true, except: :index do
