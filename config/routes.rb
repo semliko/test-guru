1b35f5770233b8b9092c432166f10c5bad3314c8
semliko <megabytus@gmail.com>
2021-08-11 09:29:51 +0100

    controller and routes for gists added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }
   root 'tests#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
@@ -20,7 +21,7 @@ Rails.application.routes.draw do
   end
 
   namespace :admin do
-    resources :gists, only: %i[index show]
+    get 'gists/index'
     resources :tests do
       resources :questions, shallow: true do
         resources :answers, shallow: true
