68fabc2f7bfcbd7112309db9adba8538c65a3a98
semliko <megabytus@gmail.com>
2021-08-17 10:05:01 +0100

    sort tests with js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,11 +5,14 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+//= require_tree
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+
+require("utilities/sorting")
 
 Rails.start()
 Turbolinks.start()
