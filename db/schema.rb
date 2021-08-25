9cc7f0bd8b35073eb6a6531c30881c3a1a9a74c6
semliko <megabytus@gmail.com>
2021-08-25 13:13:37 +0100

    postgresql added

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_08_10_114647) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
     t.boolean "correct", default: false
