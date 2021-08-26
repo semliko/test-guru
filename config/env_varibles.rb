3bc6995f249caf4f4fd54dd7e362876a1d45c7d9
semliko <megabytus@gmail.com>
2021-08-26 18:40:18 +0100

    production credentials setup

--- config/env_varibles.rb
+++ config/env_varibles.rb
@@ -2,3 +2,11 @@ if Rails.env.development?
   ENV['git_hub_token'] = Rails.application.credentials.config[:git_hub][:git_hub_token]
 end
 
+if Rails.env.production?
+  ENV["SECRET_KEY_BASE"] =  Rails.application.credentials[:secret_key_base]
+  ENV['git_hub_token'] = Rails.application.credentials.config[:git_hub][:git_hub_token]
+  ENV['SMTP_USERNAMNE'] = Rails.application.credentials.config[:gmail][:gmail_username]
+  ENV['SMTP_PASSWORD'] = Rails.application.credentials.config[:gmail][:gmail_password]
+end
+
+
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ Rails.application.configure do
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
 
+  config.secret_key_base = '<%= ENV["SECRET_KEY_BASE"] %>'
+
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
@@ -63,6 +65,16 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "test_guru_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'frozen-sierra-62217.herokuapp.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    addresses: 'smtp.gmail.com',
+    port: 587,
+    user_name: ENV['SMTP_USERNAMNE'],
+    password: ENV['SMTP_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true }
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
