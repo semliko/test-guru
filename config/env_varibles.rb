9b6e5425a2de928fe1f5825df73b673912b3e7d5
semliko <megabytus@gmail.com>
2021-08-27 10:05:45 +0100

    change cred settings

--- config/env_varibles.rb
+++ config/env_varibles.rb
@@ -4,9 +4,9 @@ end
 
 if Rails.env.production?
   ENV["SECRET_KEY_BASE"] = Rails.application.credentials[:secret_key_base]
-  #  ENV['git_hub_token'] = Rails.application.credentials.config[:git_hub][:git_hub_token]
-  #  ENV['SMTP_USERNAMNE'] = Rails.application.credentials.config[:gmail][:gmail_username]
-  #  ENV['SMTP_PASSWORD'] = Rails.application.credentials.config[:gmail][:gmail_password]
+  ENV['git_hub_token'] = Rails.application.credentials.git_hub[:git_hub_token]
+  ENV['SMTP_USERNAMNE'] = Rails.application.credentials.gmail[:gmail_username]
+  ENV['SMTP_PASSWORD'] = Rails.application.credentials.gmail[:gmail_password]
 end
 
 
