9ddeebd3249432abf9c62977a163c6e429601a6b
semliko <megabytus@gmail.com>
2021-08-27 15:52:51 +0100

    production master key fix

--- config/env_varibles.rb
+++ config/env_varibles.rb
@@ -9,9 +9,5 @@ if Rails.env.production?
   ENV['SMTP_PASSWORD'] = Rails.application.credentials.gmail[:gmail_password]
 end
 
-ENV["SECRET_KEY_BASE"] = Rails.application.credentials[:secret_key_base]
-ENV['git_hub_token'] = Rails.application.credentials.git_hub[:git_hub_token]
-ENV['SMTP_USERNAMNE'] = Rails.application.credentials.gmail[:gmail_username]
-ENV['SMTP_PASSWORD'] = Rails.application.credentials.gmail[:gmail_password]
 
 
