a3cd9d85085d2f58ed8647cfae3fd8b38ed86b79
semliko <megabytus@gmail.com>
2021-09-04 19:19:23 +0100

    fix error when creating a new test

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -26,7 +26,7 @@ class Admin::TestsController < Admin::BaseController
   def create
     @test = current_user.created_tests.new(test_params)
     if @test.save
-      redirect_to @test, notice: t('.success')
+      redirect_to admin_tests_path, notice: t('.success')
     else
       render :new, status: :unprocessable_entity
     end
