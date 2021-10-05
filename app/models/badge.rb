e460a17aba4159abaffd62d834a01f3eb86e7fe9
semliko <megabytus@gmail.com>
2021-10-05 19:55:58 +0100

    chage badge validation method

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,6 +1,6 @@
 class Badge < ApplicationRecord
   has_many :user_badges
-  validates_presence_of :image_url, :name
+  validates :image_url, :name, presence: true
   validates :image_url, format: URI::regexp(%w[http https])
 
   CATEGORIES = ["All Backend tests compleated", "Test passed from the first attempt", "Level 1 compleated" ]
