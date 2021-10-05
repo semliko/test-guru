11eaf066525f1e7064c3c37bd6365d01663d3675
semliko <megabytus@gmail.com>
2021-10-05 14:25:44 +0100

    fix root_path

--- app/helpers/nav_helper.rb
+++ app/helpers/nav_helper.rb
@@ -2,7 +2,11 @@
 module NavHelper
 
   def user_home_path
-    current_user.admin? ? admin_tests_path : root_path
+    path = root_path
+    if user_signed_in?
+      path = current_user.admin? ? admin_tests_path : root_path
+    end
+    path
   end
 
 end
